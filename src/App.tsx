54b24fecc56e30ddc6bf046fc3697f00dfdcee71
johir-rayhan <johir.rayhan01@gmail.com>
2021-06-05 20:05:55 +0600

    Creating global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from './components/Navbar';
+import { GlobalStyle } from './globalStyle';
 
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
     </Router>
   );
